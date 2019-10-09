acd423ad4b1e00ac55cc10727651eef5bc212562
Gherzan Catalina-Sorina <cgherzan@frigoglass.com>
2019-10-09 14:07:51 +0300

    Updates

---
+++ precache-manifest.ce296e936e776905dd54dd6bd0441904.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f546f9a879916e7754ad5dfd5512dfa",
+    "url": "/react-saw/tree/master/saw-program/index.html"
+  },
+  {
+    "revision": "6d34895aa37bc764688b",
+    "url": "/react-saw/tree/master/saw-program/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "3d482d528a11519f3cad",
+    "url": "/react-saw/tree/master/saw-program/static/js/2.4a704c5d.chunk.js"
+  },
+  {
+    "revision": "6d34895aa37bc764688b",
+    "url": "/react-saw/tree/master/saw-program/static/js/main.ab7d5e14.chunk.js"
+  },
+  {
+    "revision": "632d7bb118a5e72bef3b",
+    "url": "/react-saw/tree/master/saw-program/static/js/runtime-main.7c7efa12.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-saw/tree/master/saw-program/static/media/logo.25bf045c.svg"
+  }
+]);
